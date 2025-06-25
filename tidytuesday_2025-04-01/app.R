832ead373050d82cf78bd3736ca58d52c9dc0497
Ishita Khanna <ishitakhanna2208@gmail.com>
2025-06-26 00:47:07 +1000

    changed the dataset to long format

--- tidytuesday_2025-04-01/app.R
+++ tidytuesday_2025-04-01/app.R
@@ -3,6 +3,13 @@ library(tidyverse)
 
 pokemon_df <- read_csv("tidytuesday_2025-04-01/data/pokemon_df.csv")
 
+df_long <- pokemon_df %>%
+  pivot_longer(
+    cols = c(hp, attack, defense, special_attack, special_defense, speed),
+    names_to = "stat",
+    values_to = "value"
+  )
+
 ui <- fluidPage(
   titlePanel("Pokemon Stat Dashboard - TidyTuesday 01-04-2025"),
   fluidRow(
